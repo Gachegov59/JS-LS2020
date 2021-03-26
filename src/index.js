213e82e2511f84e1aced4ef56fb6bb75a51ec208
alex.g <gachegov59@gmail.com>
2021-03-26 13:48:16 +0500

    vk_map| карта рабочая

--- src/index.js
+++ src/index.js
@@ -1,74 +1,97 @@
 /*eslint-disable */
 import {auth, vkAPI} from './vk.js'
+import {customItemContentLayout} from './template.js'
 
-let myMap
-let clusterer
+document.addEventListener('DOMContentLoaded', () => {
 
-ymaps.ready(async () => {
-    const cache = new Map();
+    let myMap
+    let clusterer
+    let btn = document.querySelector('.load-vk')
 
-    await auth()
+    ymaps.ready(async () => {
 
-    const [me] = await vkAPI('users.get', {fields: 'city, country'})
-    const friends = await vkAPI('friends.get', {fields: 'city, country'})
+        btn.addEventListener('click', connectVk)
 
-    friends.items.push(me)
+        const cache = new Map();
 
-    myMap = new ymaps.Map('map', {
-        center: [58.01, 56.23],
-        zoom: 10,
-        controls: ['smallMapDefaultSet'],
-    }, {searchControlProvider: 'yandex#search'});
-    clusterer = new ymaps.Clusterer({
-        clusterDisableClickZoom: true,
-        clusterOpenBalloonOnClick: true,
-        clusterBalloonContentLayout: 'cluster#balloonCarousel'
+        myMap = new ymaps.Map('map', {
+            center: [58.01, 56.23],
+            zoom: 8,
+            controls: ['smallMapDefaultSet'],
+        }, {searchControlProvider: 'yandex#search'});
+        clusterer = new ymaps.Clusterer({
+            clusterDisableClickZoom: true,
+            clusterOpenBalloonOnClick: true,
+            clusterBalloonContentLayout: 'cluster#balloonCarousel',
+            clusterBalloonItemContentLayout: customItemContentLayout,
+        })
+
+        async function connectVk() {
+            await auth()
+            const [me] = await vkAPI('users.get', {fields: 'city, country'})
+            const friends = await vkAPI('friends.get', {fields: 'city,relation,domain, country,bdate, domain, online, photo_100, photo_200'})
+
+            friends.items.push(me)
+            myMap.geoObjects.add(clusterer)
+
+            function geocode(address) {
+                if (cache.has(address)) {
+                    return cache.get(address)
+                }
+
+                const geocodePromise = ymaps.geocode(address)
+                    .then(result => {
+                        const points = result.geoObjects.toArray();
+
+                        if (points.length) {
+                            return points[0].geometry.getCoordinates();
+                        }
+                    })
+                cache.set(address, geocodePromise)
+
+                return cache.get(address)
+            }
+
+            friends.items
+                .filter(friend => friend.first_name !== 'DELETED' && friend.country && friend.country.title)
+                .map(friend => {
+                    let friendInfo = {
+                        name: friend.first_name,
+                        lastName: friend.last_name,
+                        photo: friend.photo_100,
+                        photoBig: friend.photo_200,
+                        online: friend.online,
+                        domain: friend.domain,
+                    }
+
+                    let place = friend.country.title
+                    if (friend.city) {
+                        place += ' ' + friend.city.title
+                    }
+                    return {place, friendInfo}
+                })
+                .map(async friendObj => {
+                    const coord = await geocode(friendObj.place)
+                    const placemark = new ymaps.Placemark(coord, {
+                        friendInfo: friendObj.friendInfo,
+                        hintContent:
+                            `<a href="https://vk.com/${friendObj.friendInfo.domain}" target="_blank">` +
+                            `<div class='a_name'>${friendObj.friendInfo.name}</div> ` +
+                            `<div style="margin-bottom: 5px" class='a_name'>${friendObj.friendInfo.lastName}</div> ` +
+                            '</a>' +
+                            `<div class="ava ${friendObj.friendInfo.photo}">` +
+                            `<div class='${friendObj.friendInfo.online ? 'ava online' : 'ava'}'>` +
+                            `<img class=ballon_body src="${friendObj.friendInfo.photo}">` +
+                            '</div>' +
+                            '</div>'
+                    }, {preset: 'islands#blueHomeCircleIcon'})
+                    clusterer.add(placemark)
+                    myMap.setBounds(clusterer.getBounds());
+                })
+        }
     })
 
-    myMap.geoObjects.add(clusterer)
-
-    function geocode(address) {
-        if (cache.has(address)) {
-            return cache.get(address)
-        }
-
-        const geocodePromise = ymaps.geocode(address)
-            .then(result => {
-                const points = result.geoObjects.toArray();
-
-                if (points.length) {
-                    return points[0].geometry.getCoordinates();
-                }
-            })
-        cache.set(address, geocodePromise)
-
-        return cache.get(address)
-    }
-
-
-    console.log('friends.items', friends.items)
-
-    friends.items
-        .filter(friend => friend.first_name !== 'DELETED' && friend.country && friend.country.title)
-        .map(friend => {
-            let parts = friend.country.title
-            if (friend.city) {
-                parts += ' ' + friend.city.title
-            }
-            return parts
-        })
-        .map(async address => {
-            const coord = await geocode(address)
-            const placemark = new ymaps.Placemark(coord, {}, {preset: 'islands#blueHomeCircleIcon'})
-            clusterer.add(placemark)
-            // myMap.setBounds(myMap.geoObjects.getBounds());
-            myMap.setBounds(clusterer.getBounds());
-        })
-
-
 })
-
-
 
 
 
--- src/vk.js
+++ src/vk.js
@@ -1,19 +1,30 @@
 /* eslint-disable */
-VK.init({
-    apiId: 7782703
-})
+if (location.hostname === 'localhost') {
+    VK.init({
+        apiId: 7802630
+    })
+} else {
+    VK.init({
+        apiId: 7782703
+    })
+
+}
 
 export function auth() {
     return new Promise((res, rej) => {
         VK.Auth.login(data => {
+
             if (data.session) {
+                console.log(data)
                 res();
             } else {
+                console.log(data)
                 rej(new Error('не удалось авторизоватсья'))
             }
         }, 2)
     })
 }
+
 export function vkAPI(method, params) {
     params.v = '5.76';
 
